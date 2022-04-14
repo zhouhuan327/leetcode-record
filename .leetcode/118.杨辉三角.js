20a151165568a42e2a612aaa7ed3890398007fdd
zhouhuan <zsean21598@163.com>
2022-04-14 23:49:22 +0800

    4.14

---
+++ .leetcode/118.杨辉三角.js
@@ -0,0 +1,67 @@
+/*
+ * @lc app=leetcode.cn id=118 lang=javascript
+ *
+ * [118] 杨辉三角
+ *
+ * https://leetcode-cn.com/problems/pascals-triangle/description/
+ *
+ * algorithms
+ * Easy (74.56%)
+ * Likes:    739
+ * Dislikes: 0
+ * Total Accepted:    291.3K
+ * Total Submissions: 390.5K
+ * Testcase Example:  '5'
+ *
+ * 给定一个非负整数 numRows，生成「杨辉三角」的前 numRows 行。
+ *
+ * 在「杨辉三角」中，每个数是它左上方和右上方的数的和。
+ *
+ *
+ *
+ *
+ *
+ * 示例 1:
+ *
+ *
+ * 输入: numRows = 5
+ * 输出: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+ *
+ *
+ * 示例 2:
+ *
+ *
+ * 输入: numRows = 1
+ * 输出: [[1]]
+ *
+ *
+ *
+ *
+ * 提示:
+ *
+ *
+ * 1
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+var generate = function (numRows) {
+  const res = [];
+  for (let i = 0; i < numRows; i++) {
+    const len = i + 1;
+    const row = new Array(len);
+    row[0] = 1;
+    row[len - 1] = 1;
+    for (let j = 1; j < len - 1; j++) {
+      row[j] = res[i - 1][j - 1] + res[i - 1][j];
+    }
+    res.push(row);
+  }
+  return res;
+};
+// @lc code=end
---
+++ .leetcode/566.重塑矩阵.js
@@ -0,0 +1,76 @@
+/*
+ * @lc app=leetcode.cn id=566 lang=javascript
+ *
+ * [566] 重塑矩阵
+ *
+ * https://leetcode-cn.com/problems/reshape-the-matrix/description/
+ *
+ * algorithms
+ * Easy (66.72%)
+ * Likes:    301
+ * Dislikes: 0
+ * Total Accepted:    100.2K
+ * Total Submissions: 150.2K
+ * Testcase Example:  '[[1,2],[3,4]]\n1\n4'
+ *
+ * 在 MATLAB 中，有一个非常有用的函数 reshape ，它可以将一个 m x n 矩阵重塑为另一个大小不同（r x
+ * c）的新矩阵，但保留其原始数据。
+ *
+ * 给你一个由二维数组 mat 表示的 m x n 矩阵，以及两个正整数 r 和 c ，分别表示想要的重构的矩阵的行数和列数。
+ *
+ * 重构后的矩阵需要将原始矩阵的所有元素以相同的 行遍历顺序 填充。
+ *
+ * 如果具有给定参数的 reshape 操作是可行且合理的，则输出新的重塑矩阵；否则，输出原始矩阵。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：mat = [[1,2],[3,4]], r = 1, c = 4
+ * 输出：[[1,2,3,4]]
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：mat = [[1,2],[3,4]], r = 2, c = 4
+ * 输出：[[1,2],[3,4]]
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * m == mat.length
+ * n == mat[i].length
+ * 1 <= m, n <= 100
+ * -1000 <= mat[i][j] <= 1000
+ * 1 <= r, c <= 300
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * @param {number[][]} mat
+ * @param {number} r
+ * @param {number} c
+ * @return {number[][]}
+ */
+var matrixReshape = function (mat, r, c) {
+  if (!mat.length) return mat;
+  const row = mat.length;
+  const column = mat[0].length;
+  if (row * column !== r * c) return mat;
+
+  const flat = mat.flat();
+  const res = [];
+  // 遍历行数,截取出列数
+  for (let i = 0; i < r; i++) {
+    res.push(flat.splice(0, c));
+  }
+  return res;
+};
+// @lc code=end
