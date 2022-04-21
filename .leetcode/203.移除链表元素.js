6b6f867abf0a3fa4d2456921f350e1f40c64ca12
zhouhuan <zsean21598@163.com>
2022-04-21 17:18:17 +0800

    链表

---
+++ .leetcode/203.移除链表元素.js
@@ -0,0 +1,78 @@
+/*
+ * @lc app=leetcode.cn id=203 lang=javascript
+ *
+ * [203] 移除链表元素
+ *
+ * https://leetcode-cn.com/problems/remove-linked-list-elements/description/
+ *
+ * algorithms
+ * Easy (53.25%)
+ * Likes:    873
+ * Dislikes: 0
+ * Total Accepted:    339.8K
+ * Total Submissions: 637.1K
+ * Testcase Example:  '[1,2,6,3,4,5,6]\n6'
+ *
+ * 给你一个链表的头节点 head 和一个整数 val ，请你删除链表中所有满足 Node.val == val 的节点，并返回 新的头节点 。
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：head = [1,2,6,3,4,5,6], val = 6
+ * 输出：[1,2,3,4,5]
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：head = [], val = 1
+ * 输出：[]
+ *
+ *
+ * 示例 3：
+ *
+ *
+ * 输入：head = [7,7,7,7], val = 7
+ * 输出：[]
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 列表中的节点数目在范围 [0, 10^4] 内
+ * 1
+ * 0
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} val
+ * @return {ListNode}
+ */
+var removeElements = function (head, val) {
+  let preHead = new ListNode(-1, head);
+  let cur = preHead;
+
+  while (cur.next) {
+    if (cur.next.val === val) {
+      cur.next = cur.next.next;
+    } else {
+      cur = cur.next;
+    }
+  }
+  return preHead.next;
+};
+// @lc code=end
--- .leetcode/206.反转链表.js
+++ .leetcode/206.反转链表.js
@@ -14,46 +14,46 @@
  * Testcase Example:  '[1,2,3,4,5]'
  *
  * 给你单链表的头节点 head ，请你反转链表，并返回反转后的链表。
- * 
- * 
- * 
- * 
+ *
+ *
+ *
+ *
  * 示例 1：
- * 
- * 
+ *
+ *
  * 输入：head = [1,2,3,4,5]
  * 输出：[5,4,3,2,1]
- * 
- * 
+ *
+ *
  * 示例 2：
- * 
- * 
+ *
+ *
  * 输入：head = [1,2]
  * 输出：[2,1]
- * 
- * 
+ *
+ *
  * 示例 3：
- * 
- * 
+ *
+ *
  * 输入：head = []
  * 输出：[]
- * 
- * 
- * 
- * 
+ *
+ *
+ *
+ *
  * 提示：
- * 
- * 
+ *
+ *
  * 链表中节点的数目范围是 [0, 5000]
- * -5000 
- * 
- * 
- * 
- * 
+ * -5000
+ *
+ *
+ *
+ *
  * 进阶：链表可以选用迭代或递归方式完成反转。你能否用两种方法解决这道题？
- * 
- * 
- * 
+ *
+ *
+ *
  */
 
 // @lc code=start
@@ -68,17 +68,16 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
-var reverseList = function(head) {
-    let pre = null
-    let cur = head
-    while(cur) {
-        const next = cur.next
-        cur.next = pre
+var reverseList = function (head) {
+  let pre = null;
+  let cur = head;
+  while (cur) {
+    const next = cur.next;
+    cur.next = pre;
 
-        pre = cur
-        cur = next
-    }
-    return pre
+    pre = cur;
+    cur = next;
+  }
+  return pre;
 };
 // @lc code=end
-
